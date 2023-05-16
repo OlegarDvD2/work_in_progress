26945b5c61566ffbdee066c18f06c5ac90c8e6ad
OlegarDvD2 <83181753+OlegarDvD2@users.noreply.github.com>
2023-05-17 01:35:13 +0300

    Add files via upload

---
+++ Practise3_2/rational.h
@@ -0,0 +1,30 @@
+#ifndef R_h
+#define R_h
+
+class rational {
+public:
+    explicit rational(int a1 = 0, int b1 = 1);
+
+    void set(int a1, int b1);
+
+    void show() const;
+
+    rational operator+(const rational& other) const;
+
+    friend rational operator-(const rational& f1, const rational& f2);
+
+    void operator++();
+
+    bool operator==(const rational& other) const;
+
+    bool operator>(const rational& other) const;
+
+    rational& operator=(const rational& other);
+
+    void reduce();
+
+private:
+    int numerator;
+    int denominator;
+};
+#endif
